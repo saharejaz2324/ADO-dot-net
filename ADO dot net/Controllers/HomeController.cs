64f42b82bea11f061ad08eb7f05cad35f229948e
sej <sej@ciklum.com>
2020-09-16 11:37:24 +0500

    Insert data in database and get that data by using SQL

---
+++ ADO dot net/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using ADO_dot_net.DB;
+using ADO_dot_net.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace ADO_dot_net.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        Database db = new Database();
+
+        [HttpPost]
+        public IActionResult insertData(Inventory val)
+        {
+            string query = "INSERT INTO Inventory (Name, Price, Quantity) VALUES (@Name, @Price, @Quantity);";
+            var parameters = new IDataParameter[]
+            {
+               new SqlParameter("@Name", val.Name),
+               new SqlParameter("@Price", val.Price),
+               new SqlParameter("@Quantity", val.Quantity)
+           };
+            if (db.ExecuteData(query, parameters) > 0)
+            {
+                return Ok("Inserted");
+            }
+            else
+            {
+                return NotFound("Something went Wrong!!");
+            }
+
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+      {
+            string query = "select * from Inventory";
+            DataTable dt = db.GetData(query);
+            var result = new ObjectResult(dt);
+            return result;
+        }
+    }
+}
---
+++ ADO dot net/DB/Database.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+namespace ADO_dot_net.DB
+{
+    public class Database
+    {
+        public static string sqlDataSource = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Company;Trusted_Connection=True;";
+        //public static string sqlDataSource = "Data Source=DESKTOP-GV4424J;Initial Catalog=TestDB ; Integrated Security = True;";
+
+        public DataTable GetData(string str)
+        {
+            DataTable objresult = new DataTable();
+
+            try
+            {
+                SqlDataReader myReader;
+                using (SqlConnection mycon = new SqlConnection(sqlDataSource))
+                {
+                    mycon.Open();
+
+                    SqlCommand command = new SqlCommand(str, mycon);
+                    myReader = command.ExecuteReader();
+                        objresult.Load(myReader);
+
+                        myReader.Close();
+                        mycon.Close();
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return objresult;
+        }
+
+        public int ExecuteData(string str, params IDataParameter[] sqlParams)
+        {
+            int rows = -1;
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(sqlDataSource))
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = new SqlCommand(str, conn))
+                    {
+                        if (sqlParams != null)
+                        {
+                            foreach (IDataParameter para in sqlParams)
+                            {
+                                cmd.Parameters.Add(para);
+                            }
+                            rows = cmd.ExecuteNonQuery();
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return rows;
+        }
+    }
+}
